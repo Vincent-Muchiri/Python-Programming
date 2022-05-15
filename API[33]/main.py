ecdd35b8e1fc5426dff9877fc6f9c59f15f2d0c0
Vincent-Muchiri <vincentmuchiri.official@outlook.com>
2022-05-15 22:21:36 +0300

    Day 33[API]

--- API[33]/main.py
+++ API[33]/main.py
@@ -2,7 +2,7 @@ import requests
 
 # TODO Request a response from the endpoint URL
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status() # Raise exceptions
+response.raise_for_status() # Raise exceptions for the response code
 
 # TODO Get the actual data
 data = response.json()
