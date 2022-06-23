ebee8660f048bcab467d2e801e1aceffd2cd635b
Vincent-Muchiri <vincentmuchiri.official@outlook.com>
2022-06-23 11:01:27 +0300

    Added some values to the environment Failed to send whatsapp messages

--- API[33, 35]/Rain Alert App[Twilio, Whatsapp, API Auth, env, Replit Hosting]/main.py
+++ API[33, 35]/Rain Alert App[Twilio, Whatsapp, API Auth, env, Replit Hosting]/main.py
@@ -5,16 +5,17 @@ from datetime import datetime, timedelta
 from twilio.rest import Client
 # import pywhatkit
 
-LONG = 37.014404
-LAT = -1.101822
+LONG = os.environ['longitude']
+LAT = os.environ['latitude']
 
-KEY = "4f8d0b9b29c3024b7ccb872efff7a70b"
+KEY = os.environ['openweather_api_key']
 
 # TODO Call the Twilio API
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = "ACd8d21a162d64bcdbba53d8e76c711c0e"
-auth_token = "7c67a13a2dcaa67ca75179fe585e38f8"
+account_sid = os.environ['twilio_account_sid']
+auth_token = os.environ['twilio_auth_token']
+
 
 # TODO Call the weather API
 url = f"https://api.openweathermap.org/data/2.5/onecall?" \
@@ -23,23 +24,23 @@ url = f"https://api.openweathermap.org/data/2.5/onecall?" \
       f"exclude=current,minutely,daily,alerts&" \
       f"appid={KEY}"
 
-# response = requests.get(url)
-# response.raise_for_status()
-#
-# weather_api_data = response.json()
-#
-# # TODO Save the data in a JSON file
-# try:
-#     os.mkdir("data")
-# except FileExistsError:
-#     pass
-#
-# with open("./data/test.json", mode="w") as json_file:
-#     json.dump(weather_api_data, json_file, indent=4)
+response = requests.get(url)
+response.raise_for_status()
+
+weather_api_data = response.json()
+
+# TODO Save the data in a JSON file
+try:
+    os.mkdir("data")
+except FileExistsError:
+    pass
+
+with open("./data/test.json", mode="w") as json_file:
+    json.dump(weather_api_data, json_file, indent=4)
 
 # TODO Offline testing
-with open("./data/test.json", mode="r") as json_file:
-    weather_api_data = json.load(json_file)
+# with open("./data/test.json", mode="r") as json_file:
+#     weather_api_data = json.load(json_file)
 
 will_rain = False
 
@@ -51,23 +52,24 @@ for hour_data in hourly_list:
     for condition in weather_conditions_list:
         weather_code = condition['id']
         # print(weather_code)
-        if weather_code > 700:
+        if weather_code < 700:
             will_rain = True
             # TODO Get the timestamp and convert it to local time
             timestamp = hour_data['dt']
-            time_dt = datetime.fromtimestamp(timestamp).time()
+            time_dt = datetime.fromtimestamp(timestamp)
+            # time_dt = time_dt.time() # For replit
 
             # TODO Get the description
             weather_description = condition['description'].capitalize()
 
-            # print(f"{weather_description} forecasted at {time_dt}. Carry an umbrella.")
+            print(f"{weather_description} forecasted at {time_dt}. Carry an umbrella.")
 
             # TODO Send the SMS
             client = Client(account_sid, auth_token)
 
             message_contents = f"{weather_description} forecasted at {time_dt}. Carry an umbrella."
-            from_no = '+19897873943'
-            to_no = '+254798794666'
+            from_no = os.environ['twilio_from_no']
+            to_no = os.environ['twilio_to_no']
 
             message = client.messages \
                 .create(
@@ -85,9 +87,7 @@ for hour_data in hourly_list:
             # pywhatkit.sendwhatmsg(to_no,
             #                       message_contents,
             #                       sending_time_hr, sending_time_min)
-            # pywhatkit.sendwhatmsg('+25479001219',
-            #                       message_contents,
-            #                       sending_time_hr, sending_time_min)
+
     break
 
 # if will_rain:
