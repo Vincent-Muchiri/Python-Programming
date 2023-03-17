d874b09c737d20e5ae279a86e3f9b07fbf1219c5
Vincent Muchiri <99247513+Vincent-Muchiri@users.noreply.github.com>
2023-03-17 13:24:37 +0300

    feat!: run Selenium test concurrently using a function

--- Internet Speed Twitter-Complaint Bot [52]/main.py
+++ Internet Speed Twitter-Complaint Bot [52]/main.py
@@ -1,4 +1,49 @@
 from internetSpeedTwitterBot import InternetSpeedTwitterBot
+from concurrent.futures import as_completed, ThreadPoolExecutor
+from pprint import pprint
 
 bot = InternetSpeedTwitterBot()
-bot.get_internet_speed()
+
+
+# TODO Run multiple tests concurrently
+
+def test_multiple_times():
+    futures, download_speeds_array, upload_speeds_array = [], [], []
+
+    with ThreadPoolExecutor(max_workers=3) as executor:
+        for tests in range(3):
+            f = executor.submit(bot.get_internet_speed)
+            futures.append(f)
+
+    print(len(futures))
+
+    for future in as_completed(futures):
+        # TODO Get the speeds
+        internet_speed_dict = future.result()
+
+        download_speeds = internet_speed_dict['download_speed']
+        download_units = internet_speed_dict['download_units']
+        upload_speeds = internet_speed_dict['upload_speed']
+        upload_units = internet_speed_dict['upload_units']
+
+        # TODO Convert all speeds to Kbps
+        if download_units == "Mbps":
+            download_speeds = download_speeds * 1000
+        if upload_units == "Mbps":
+            upload_speeds = upload_speeds * 1000
+
+        # TODO Append the speeds to a list
+        download_speeds_array.append(download_speeds)
+        upload_speeds_array.append(upload_speeds)
+
+    print(download_speeds_array)
+    print(upload_speeds_array)
+
+    # TODO Check whether there's a speed that is way off and remove it
+
+    # TODO Calculate the average speed
+
+def test_ones():
+    internet_speed_dict = bot.get_internet_speed()
+    pprint(internet_speed_dict)
+
