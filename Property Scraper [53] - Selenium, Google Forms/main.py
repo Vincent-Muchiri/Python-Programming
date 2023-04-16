4dc93eb796f9e4a3e72f742afaf4d3e404996a8c
Vincent Muchiri <99247513+Vincent-Muchiri@users.noreply.github.com>
2023-04-16 15:39:49 +0300

    feat: fill data to a Google form

--- Property Scraper [53] - Selenium, Google Forms/main.py
+++ Property Scraper [53] - Selenium, Google Forms/main.py
@@ -6,4 +6,8 @@ from propertyScraper import PropertyScraper
 property_scraper_bot = PropertyScraper()
 
 # TODO Scrap the property data and store it in a dict
-scraped_property_details_dict = property_scraper_bot.get_property_data()
+scraped_data_dict = property_scraper_bot.get_property_data()
+print(scraped_data_dict)
+
+# TODO Fill in the Google sheet form using Selenium
+property_scraper_bot.fill_property_data(scraped_data_dict)
--- Property Scraper [53] - Selenium, Google Forms/propertyScraper.py
+++ Property Scraper [53] - Selenium, Google Forms/propertyScraper.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
-from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 import requests
 from pprint import pprint
 
@@ -9,15 +13,6 @@ class PropertyScraper:
     def __init__(self):
         self.FORM_URL = "https://forms.gle/GaCAMYw9tTpeudYK7"
         self.PROPERTY_PAGE_URL = "https://www.property24.co.ke/1-bedroom-apartments-flats-to-rent-in-nairobi-c1890?toprice=20000"
-
-        # TODO Create webdriver options
-        driver_options = Options()
-
-        # TODO Set headless option to True or False
-        driver_options.headless = True
-
-        # TODO Instantiate webdriver
-        chrome_driver = webdriver.Chrome(executable_path="../chromedriver.exe", options=driver_options)
 
 
     def get_property_data(self):
@@ -72,9 +67,55 @@ class PropertyScraper:
 
         return scraped_property_details_dict
 
+    def fill_property_data(self, property_data: dict):
+        # TODO Create webdriver options
+        driver_options = Options()
 
-    def fill_property_data(self):
-        pass
+        # TODO Set headless option to True or False
+        driver_options.headless = False
+
+        # TODO Instantiate webdriver
+        chrome_driver = webdriver.Chrome(executable_path="../chromedriver.exe", options=driver_options)
+
+        # TODO Open the form online
+        chrome_driver.get(url=self.FORM_URL)
+
+        # TODO Get all the inputs element objects
+        all_input_elems_array = chrome_driver.find_elements(By.TAG_NAME, 'input')
+        # print(input_elems)
+        input_elems_array = []
+        for input_elem in all_input_elems_array:
+            if input_elem.get_attribute('type') == "text":
+                input_elems_array.append(input_elem)
+
+        address_array = property_data['address']
+        rental_price_array = property_data['rental price']
+        link_array = property_data['link']
+
+        # TODO Enter the data to the form
+        for property_index in range(len(address_array)):
+            # TODO Get all the inputs element objects
+            all_input_elems_array = chrome_driver.find_elements(By.TAG_NAME, 'input')
+            # print(input_elems)
+            input_elems_array = []
+            for input_elem in all_input_elems_array:
+                if input_elem.get_attribute('type') == "text":
+                    input_elems_array.append(input_elem)
+
+            input_elems_array[0].send_keys(address_array[property_index])
+            input_elems_array[1].send_keys(rental_price_array[property_index])
+            input_elems_array[2].send_keys(link_array[property_index])
+
+            # TODO Click submit button
+            submit_btn = chrome_driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
+            submit_btn.click()
+
+            # TODO Click the link to a new response
+            new_response_btn = WebDriverWait(chrome_driver, 30).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[2]/div[1]/div/div[4]/a')))
+            new_response_btn.click()
+
+
+
 
     def form_to_excel(self):
         pass
